87a63a8de99af241b7bac85e45b6f88db90348ca
safuraK <safurakasu@gmail.com>
2025-06-18 13:09:55 +0300

    1 qubit test mode

--- gates.py
+++ gates.py
@@ -1,14 +1,23 @@
 from qiskit import QuantumCircuit
-from IPython.display import display
+from qiskit.quantum_info import SparsePauliOp
+from qiskit.transpiler import generate_preset_pass_manager
 import matplotlib.pyplot as plt
+from qiskit import QuantumCircuit, Aer, execute
+from qiskit.visualization import plot_bloch_vector
+from qiskit.quantum_info import Statevector
 
-circuit = QuantumCircuit(1)
+#Later to be integrated via streamlit. Asking for user input.
+number_of_qubits = int(input("How many qubits do you wish to play with? "))
 
-circuit.h(0)
-circuit.t(0)
-circuit.h(0)
-circuit.s(0)
-circuit.y(0)
+#To be made more dynamic after addition of streamlit. But set to 0 for the initial testing phase.
+initial_state = 0
 
-display(circuit.draw(output="mpl"))
+# Create a new circuit with two qubits
+qc = QuantumCircuit(number_of_qubits)
+ 
+# Add a Hadamard gate to qubit of user's choice.
+final_state = qc.h(initial_state)
+
+#Test
+qc.draw("mpl")
 plt.show()
