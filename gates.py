74f2c826a43d9eca358ff71394834e5718a9b8b3
safuraK <safurakasu@gmail.com>
2025-06-18 20:13:09 +0300

    Initial bloch sphere implementation

--- gates.py
+++ gates.py
@@ -2,22 +2,32 @@ from qiskit import QuantumCircuit
 from qiskit.quantum_info import SparsePauliOp
 from qiskit.transpiler import generate_preset_pass_manager
 import matplotlib.pyplot as plt
-from qiskit import QuantumCircuit, Aer, execute
-from qiskit.visualization import plot_bloch_vector
-from qiskit.quantum_info import Statevector
+from qiskit.visualization import plot_bloch_multivector
+from qiskit.quantum_info import Statevector, DensityMatrix
 
 #Later to be integrated via streamlit. Asking for user input.
-number_of_qubits = int(input("How many qubits do you wish to play with? "))
+#number_of_qubits = int(input("How many qubits do you wish to play with? "))
 
 #To be made more dynamic after addition of streamlit. But set to 0 for the initial testing phase.
-initial_state = 0
+initial_state = 0 #####
 
 # Create a new circuit with two qubits
-qc = QuantumCircuit(number_of_qubits)
- 
-# Add a Hadamard gate to qubit of user's choice.
-final_state = qc.h(initial_state)
+qc = QuantumCircuit(1)
 
+
+# Add a Hadamard gate to qubit of user's choice.
+qc.x(0)
+#qc.h(0)
+
+# Get the statevector.
+state = Statevector.from_instruction(qc)
+
+plot_bloch_multivector(state.data, title="Bloch Sphere via plot_bloch_multivector")
+plt.show()
+
+'''
 #Test
 qc.draw("mpl")
-plt.show()
+'''
+
+
